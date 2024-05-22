e421ba1733b1cf1679a24ebe267a1d845a2f57a5
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2024-05-22 11:34:39 +0200

    Send browser finger print with traffic analytics

--- scripts/a.js
+++ scripts/a.js
@@ -1,15 +1,25 @@
+const fp = new Fingerprint({
+    canvas: true,
+    ie_activex: true,
+    screen_resolution: true
+});
+const hash = fp.get() + '-' + fp.murmurhash3_32_gc(all.join('###'), 31);
+
+const payload = {
+    d: (new Date()).toISOString(),
+    r: window.location.href,
+    f: document.referrer || null,
+    w: window.innerWidth,
+    h: hash
+}
+
 const option = {
     method: 'POST',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-        d: (new Date()).toISOString(),
-        r: window.location.href,
-        f: document.referrer || null,
-        w: window.innerWidth
-    }),
+    body: JSON.stringify(payload),
 };
 
-fetch('https://octopus-app-zzeuv.ondigitalocean.app/traffic', { ...option });
+fetch('http://localhost:3000/traffic', { ...option });
